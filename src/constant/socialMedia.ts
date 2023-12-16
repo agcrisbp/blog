e09568d814db4d014c30fd5e58b15cf01698d747
agcrisbp <agcrisbp@email.com>
2023-12-16 07:48:28 +0700

    fix Twitter

--- src/constant/socialMedia.ts
+++ src/constant/socialMedia.ts
@@ -10,7 +10,7 @@ export const socialMedia: Array<SocialMediaProps> = [
   },
   {
     children: 'Twitter',
-    href: 'https://twitter.com/agcrisbp'
+    href: 'https://twitter.com/crisminolog'
   },
   {
     children: 'Linkedin',
