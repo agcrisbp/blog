ec0d7314aa824239d63c2606fe2352a94bbbfb95
agcrisbp <agcrisbp@email.com>
2024-01-02 14:11:19 +0700

    fix layout

--- src/components/template/HeroWithPhoto.tsx
+++ src/components/template/HeroWithPhoto.tsx
@@ -21,6 +21,9 @@ const HeroWithPhoto: React.FunctionComponent<HeroProps> = (props) => {
         )}
       >
         <h1 className={twclsx('text-4xl md:text-5xl')}>{props.title}</h1>
+        <figure className={twclsx('relative', 'w-20 h-20 md:w-24 md:h-24')}>
+          <CustomImage src={props.image} className={twclsx('rounded-3xl')} alt={props.imageAlt} display='flex' />
+        </figure>
       </div>
       <p className={twclsx('max-w-prose')}>{props.description}</p>
       {props.children}
