dae22b9102f7d409aa0a63e6b5d4c16ab3ef1668
agcrisbp <agcrisbp@email.com>
2025-01-05 16:58:29 +0700

    Update: New Blog, CodeBlock style, etc.

--- src/components/contents/Pre.tsx
+++ src/components/contents/Pre.tsx
@@ -7,9 +7,10 @@ import Prism from 'prismjs';
 import 'prismjs/components/';
 
 interface CodeBlock {
-  label: string
+  label?: string
   content: string
-  language: string
+  language?: string
+  iframe?: boolean
 }
 
 interface PreProps {
@@ -23,6 +24,7 @@ const Pre: React.FunctionComponent<PreProps> = ({ codeBlocks }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false)
   const dropdownRef = useRef<HTMLDivElement>(null)
   const preRef = useRef<HTMLPreElement>(null)
+  const iframeRef = useRef<HTMLIFrameElement>(null)
 
   const tooltipProps = {
     title: isCopied ? 'Tersalin!' : 'Salin ke Papan Klip',
@@ -52,49 +54,42 @@ const Pre: React.FunctionComponent<PreProps> = ({ codeBlocks }) => {
     }
 
     document.addEventListener('click', handleClickOutside)
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside)
-    }
+    return () => document.removeEventListener('click', handleClickOutside)
   }, [])
 
   useEffect(() => {
-    if (preRef.current) {
-      Prism.highlightElement(preRef.current as HTMLElement);
+    if (!codeBlocks[activeIndex].iframe && preRef.current) {
+      Prism.highlightElement(preRef.current as HTMLElement)
     }
-  }, [activeIndex]);
+  }, [activeIndex])
 
-  const currentContent = codeBlocks[activeIndex].content
-  const language = codeBlocks[activeIndex].language
-  const lineCount = currentContent.split('\n').length
-  const shouldShowToggle = lineCount > 3
-  const shouldShowDropdown = codeBlocks.length > 1
+  const currentBlock = codeBlocks[activeIndex]
+  const language = currentBlock.language || 'plaintext'
+
+  useEffect(() => {
+    if (currentBlock.iframe && iframeRef.current) {
+      const iframe = iframeRef.current
+      iframe.onload = () => {
+        if (iframe.contentWindow?.document.body) {
+          iframe.style.height = `${iframe.contentWindow.document.body.scrollHeight}px`
+        }
+      }
+    }
+  }, [activeIndex])
 
   return (
     <div className={twclsx('relative')}>
-      <div
-        className={twclsx(
-          'absolute left-0 right-12',
-          'h-11 rounded-tl rounded-br',
-          'font-semibold text-sm',
-          'bg-slate-700 dark:bg-slate-800 text-main-1.5'
-        )}
-      >
-        <div
-          className={twclsx('inline-flex items-center justify-start', 'px-4 md:px-1 h-full rounded-tl', 'bg-primary-4')}
-        >
-          {shouldShowDropdown && (
+      {!currentBlock.iframe && (
+        <div className={twclsx('absolute left-0 right-12', 'h-11 rounded-tl rounded-br', 'bg-slate-700 dark:bg-slate-800')}>
+          {codeBlocks.length > 1 && (
             <div className="relative" ref={dropdownRef}>
               <button
-                className={twclsx(
-                  'bg-primary-4 text-white py-1 px-3 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500'
-                )}
+                className={twclsx('bg-primary-4 text-white py-2 px-3 rounded-md shadow-sm')}
                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
               >
-                {codeBlocks[activeIndex].label}
+                {currentBlock.label || 'Code Block'}
                 <ChevronDownIcon className="w-4 h-4 ml-2 inline-block" />
               </button>
-
               {isDropdownOpen && (
                 <div className="absolute mt-1 left-0 right-0 bg-primary-4 shadow-md rounded-md z-10">
                   {codeBlocks.map((block, index) => (
@@ -106,7 +101,7 @@ const Pre: React.FunctionComponent<PreProps> = ({ codeBlocks }) => {
                       }}
                       className="w-full text-left px-4 py-2 hover:bg-gray-100 focus:outline-none"
                     >
-                      {block.label}
+                      {block.label || `Block ${index + 1}`}
                     </button>
                   ))}
                 </div>
@@ -114,59 +109,66 @@ const Pre: React.FunctionComponent<PreProps> = ({ codeBlocks }) => {
             </div>
           )}
         </div>
-      </div>
+      )}
 
-      <div
-        className={twclsx(
-          'absolute top-0 right-0',
-          'flex items-center justify-center',
-          'w-11 h-11 rounded-tr rounded-bl',
-          'bg-slate-700 dark:bg-slate-800'
-        )}
-      >
-        <Tooltip {...tooltipProps}>
-          <Button
-            onClick={copyToClipboard}
-            screenReaderText='Salin Ke Papan Klip'
-            className={twclsx(
-              'group relative',
-              'inline-flex items-center justify-center',
-              'w-8 h-8 rounded-lg transition-all duration-200',
-              'ring-main-2',
-              'hover:ring'
-            )}
-          >
-            <ClipboardCopyIcon className='w-4 h-4 text-main-1' />
-          </Button>
-        </Tooltip>
-      </div>
-
-      {/* Apply the PrismJS language class */}
-      <pre
-        ref={preRef}
-        className={twclsx(
-          'pt-[3.5rem!important]',
-          !isExpanded && shouldShowToggle ? 'max-h-32 overflow-hidden' : '',
-          `language-${language}` // Dynamically add the language class
-        )}
-      >
-        {currentContent}
-      </pre>
-
-      {shouldShowToggle && (
-        <button
-          onClick={() => setIsExpanded(!isExpanded)}
+      {!currentBlock.iframe && (
+        <div
           className={twclsx(
-            'absolute right-2 bottom-2 p-1 rounded-full bg-gray-700 hover:bg-gray-600',
-            'focus:outline-none focus:ring-2 focus:ring-blue-500'
+            'absolute top-0 right-0',
+            'flex items-center justify-center',
+            'w-11 h-11 rounded-tr rounded-bl',
+            'bg-slate-700 dark:bg-slate-800'
           )}
         >
-          {isExpanded ? (
-            <ChevronUpIcon className="w-5 h-5 text-white" />
-          ) : (
-            <ChevronDownIcon className="w-5 h-5 text-white" />
+          <Tooltip {...tooltipProps}>
+            <Button
+              onClick={copyToClipboard}
+              screenReaderText='Salin Ke Papan Klip'
+              className={twclsx(
+                'group relative',
+                'inline-flex items-center justify-center',
+                'w-8 h-8 rounded-lg transition-all duration-200',
+                'ring-main-2 hover:ring'
+              )}
+            >
+              <ClipboardCopyIcon className='w-4 h-4 text-main-1' />
+            </Button>
+          </Tooltip>
+        </div>
+      )}
+
+      {currentBlock.iframe ? (
+        <iframe
+          ref={iframeRef}
+          sandbox="allow-scripts allow-same-origin"
+          srcDoc={currentBlock.content}
+          className={twclsx(
+            'w-full border rounded-md max-h-full'
           )}
-        </button>
+        />
+      ) : (
+        <pre
+          ref={preRef}
+          className={twclsx(
+            'pt-[3.5rem!important]',
+            !isExpanded && currentBlock.content.split('\n').length > 3 ? 'max-h-32 overflow-hidden' : '',
+            `language-${language}`
+          )}
+        >
+          {currentBlock.content}
+        </pre>
+      )}
+
+      {!currentBlock.iframe && (
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        className={twclsx(
+          'absolute right-2 bottom-2 p-1 rounded-full bg-gray-700 hover:bg-gray-600',
+          'focus:outline-none focus:ring-2 focus:ring-blue-500'
+        )}
+      >
+        {isExpanded ? <ChevronUpIcon className="w-5 h-5 text-white" /> : <ChevronDownIcon className="w-5 h-5 text-white" />}
+      </button>
       )}
     </div>
   )
