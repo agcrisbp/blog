637d3f4aa36d4a0ffa0fbf44a2b9639405313225
agcrisbp <agcrisbp@email.com>
2025-01-09 13:51:52 +0700

    main

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -16,8 +16,8 @@ const NotFoundPage: NextPage = () => {
       <section className={twclsx('flex flex-col items-center justify-center', '-mt-16 md:-mt-20', 'min-h-screen')}>
         <h1 className={twclsx('text-center')}>404 | Not Found</h1>
         <p className={twclsx('text-center max-w-prose', 'my-4')}>
-          Halaman yang kamu cari tidak ditemukan, jika menurutmu ini sebuah kesalahan, tolong hubungi kami di{' '}
-          <em>agcrisbp@email.com</em>
+          Halaman yang kamu cari tidak ditemukan, jika menurutmu ini sebuah kesalahan, tolong hubungi kami melalui{' '}
+          <UnderlineLink href="https://aghea.vercel.app/security">email</UnderlineLink>.
         </p>
         <UnderlineLink href='/'>Kembali Ke Beranda</UnderlineLink>
       </section>
